996913a7af2bbc9d50fe1e793f71ec00fefa4313
przemyslaw <zawadkaprzemek@gmail.com>
2023-05-22 23:31:14 +0200

    tajne głosowania, przebieg

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -42,6 +42,7 @@ class ExceptionListener
               'line'=>$exception->getLine()
           ]
         );
+        dump($exception);
 
         $this->em->persist($log);
         $this->em->flush();
